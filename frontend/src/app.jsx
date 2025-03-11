d848169eb1d9b53b6a4cfd1e1a0ac30260a834f8
IoannaDamianov <damianov.io@northeastern.edu>
2025-03-11 11:52:22 -0400

    reorganized files and began converting question_display html to react - Ioanna

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Header from './header;
-import ScoreCounter from './score-counter';
+import Header from './components/header'
+import ScoreCounter from './components/score-counter';
 
 document.addEventListener('DOMContentLoaded', () => {
     let btn = document.querySelector('.submit-btn'); // Select the submit button correctly
